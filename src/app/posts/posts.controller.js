8cd6ffa0f05d8ceef5c9d0f51d0e192694c32ade
Jose B Gomes <jose@believecreative.com>
2015-08-22 20:04:42 +0100

    Having fun making it responsive and looking like pinterest

--- src/app/posts/posts.controller.js
+++ src/app/posts/posts.controller.js
@@ -12,17 +12,6 @@
     vm.posts = posts;
     vm.checkbox = false;
 
-    $scope.$watch('vm.checkbox', function() {
-      if(vm.checkbox) {
-        vm.posts = vm.posts.filter(function(post) {
-          return post.type === 'event';
-        });
-      }
-      else {
-        vm.posts = posts;
-      }
-    });
-
     $rootScope.loading = false;
   }
 
